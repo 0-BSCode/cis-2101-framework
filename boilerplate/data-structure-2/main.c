8462c3ad20dced2a022f7886bb36fc911fc5321f
0-BSCode <bryanipsanchez@gmail.com>
2023-11-06 19:20:36 +0800

    Fill out main.c files for boilerplate

---
+++ boilerplate/data-structure-2/main.c
@@ -0,0 +1,14 @@
+#include "modules/implementation-1/_header.h"
+
+// * TESTS HERE
+int main() {
+    DataStructureName DS;
+
+    initializeStruct();
+    addToStruct();
+    deleteFromStruct();
+    updateStruct();
+    displayStruct();
+
+    return 0;
+}
