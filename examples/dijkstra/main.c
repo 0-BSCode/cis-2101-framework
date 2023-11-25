9a895037c61c3bfda370351f7e5d11f88cf9d31b
0-BSCode <bryanipsanchez@gmail.com>
2023-11-25 13:50:13 +0800

    Add more test cases

--- examples/dijkstra/main.c
+++ examples/dijkstra/main.c
@@ -17,8 +17,24 @@ int main() {
         {INF, 2, INF, 2, INF}
     };
 
-    printMatrix(matrix);
-    int *minCosts = dijkstra(matrix, 0);
+    int matrix3[MAX][MAX] = {
+         {INF, 1, 4, INF, INF},
+        {1, INF, 4, 2, INF},
+        {4, 4, INF, 3, INF},
+        {INF, 2, 3, INF, 2},
+        {INF, INF, INF, 2, INF}
+     };
+
+    int matrix4[MAX][MAX] = {
+        {INF, 2, INF, 1, INF},
+        {INF, INF, 3, INF, INF},
+        {INF, INF, INF, 2, INF},
+        {INF, INF, INF, INF, 1},
+        {INF, INF, 1, INF, INF}
+     };
+
+    printMatrix(matrix3);
+    int *minCosts = dijkstra(matrix3, 0);
 
     printArray(minCosts);
 
