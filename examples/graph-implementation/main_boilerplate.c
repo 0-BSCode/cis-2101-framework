028e59f82d514a3600e17b8860be3c150719d0ab
0-BSCode <bryanipsanchez@gmail.com>
2023-12-10 10:20:48 +0800

    Added boilerplate for practice

--- examples/graph-implementation/main_boilerplate.c
+++ examples/graph-implementation/main_boilerplate.c
@@ -9,11 +9,8 @@ int main() {
     printf("\n\n\nProblem #1:: ");
     printf("\n------------\n");
     // Declare variables needed for Problem #1
-    MATRIX m;
 
     // Function Calls for Problem #1
-    initializeMatrix(m);
-    displayMatrix(m);
 
     /*---------------------------------------------------------------------------------
      * 	Problem #2 ::  1) Populates the matrix * 2)
@@ -24,8 +21,7 @@ int main() {
     // Declare variables needed for Problem #2
 
     // Function Calls for Problem #2
-    populateMatrix(m);
-    displayMatrix(m);
+
     /*---------------------------------------------------------------------------------
      * 	Problem #3 ::  1) Converts the matrix into a list * 2)
      *  Displays the list        *
@@ -33,10 +29,9 @@ int main() {
     printf("\n\n\nProblem #3:: ");
     printf("\n------------\n");
     // Declare variables needed for Problem #3
-    LIST *L;
+
     // Function Calls for Problem #3
-    L = convertToList(m);
-    displayList(*L);
+
     /*---------------------------------------------------------------------------------
      * 	Problem #4 ::  1) Initializes the virtual heap and cursor-based graph * 2)
      *  Displays the empty virtual heap       *
@@ -44,11 +39,8 @@ int main() {
     printf("\n\n\nProblem #4:: ");
     printf("\n------------\n");
     // Declare variables needed for Problem #4
-    VHeap VH;
 
     // Function Calls for Problem #4
-    VH = initVHeap();
-    displayVHeap(VH);
 
     /*---------------------------------------------------------------------------------
      * 	Problem #5 ::  1) Initializes the cursor-based graph * 2)
@@ -57,11 +49,8 @@ int main() {
     printf("\n\n\nProblem #5:: ");
     printf("\n------------\n");
     // Declare variables needed for Problem #5
-    CURSOR CS;
 
     // Function Calls for Problem #5
-    initCursor(&CS, VH);
-    displayCursor(CS);
 
     /*---------------------------------------------------------------------------------
      * 	Problem #6 ::  1) Converts the list to a cursor-based graph * 2)
@@ -72,9 +61,6 @@ int main() {
     // Declare variables needed for Problem #6
 
     // Function Calls for Problem #6
-    convertToCursor(L, &CS);
-    displayCursor(CS);
-    displayVHeap(CS.VH);
 
     /*---------------------------------------------------------------------------------
      * 	Problem #7 ::  1) Initializes a new adjacency matrix* 2)
@@ -83,10 +69,8 @@ int main() {
     printf("\n\n\nProblem #7:: ");
     printf("\n------------\n");
     // Declare variables needed for Problem #7
-    MATRIX newMatrix;
+
     // Function Calls for Problem #7
-    initializeMatrix(newMatrix);
-    convertToMatrix(L, newMatrix);
-    displayMatrix(newMatrix);
+
     return 0;
 }
--- examples/graph-implementation/modules/boilerplate.c
+++ examples/graph-implementation/modules/boilerplate.c
@@ -1,4 +1,4 @@
-#include "../_header.h"
+#include "_header.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,29 +13,13 @@ void displayMatrix(MATRIX matrix) {
     printf("\n%-7s%15s\n", "------", "--------------------");
 
     // Insert code here
-    int x, y;
-
-    for (x = 0; x < MATRIX_MAX; x++) {
-        printf("%-7d", x);
-        for (y = 0; y < MATRIX_MAX; y++) {
-            printf("%-5d ", matrix[x][y] == INF ? -1 : matrix[x][y]);
-        }
-        printf("\n");
-    }
 
     printf("\n\n");
     system("Pause");
 }
 
 void initializeMatrix(MATRIX matrix) {
-    // Code here
-    int x, y;
-
-    for (x = 0; x < MATRIX_MAX; x++) {
-        for (y = 0; y < MATRIX_MAX; y++) {
-            matrix[x][y] = -1;
-        }
-    }
+    // Insert code here
 }
 
 /************************************************************
@@ -50,13 +34,7 @@ void populateMatrix(MATRIX matrix) {
         {INF, INF, INF, INF, INF}
     };
 
-    // Logic here
-    int x, y;
-    for (x = 0; x < MATRIX_MAX; x++) {
-        for (y = 0; y < MATRIX_MAX; y++) {
-            matrix[x][y] = data[x][y];
-        }
-    }
+    // Insert code here
 }
 
 /************************************************************
@@ -70,47 +48,15 @@ void displayList(LIST list) {
     printf("\n%-7s%15s\n", "------", "--------------------");
 
     // Insert code here
-    int x;
-    Node trav;
-    for (x = 0; x < OPEN_MAX; x++) {
-        printf("%-7d", x);
-        for (trav = list[x]; trav != NULL; trav = trav->edge) {
-            printf("%-2d (%-3d) | ", trav->vertex, trav->cost == INF ? -1 : trav->cost);
-        }
-        printf("\n");
-    }
+
     printf("\n\n");
     system("Pause");
 }
 void initList(LIST *L) {
-    int x;
-
-    for (x = 0; x < OPEN_MAX; x++) {
-        (*L)[x] = NULL;
-    }
+    // Insert code here
 }
 LIST *convertToList(MATRIX matrix) {
-    LIST *L = (LIST *)malloc(sizeof(LIST));
-    if (L != NULL) {
-        initList(L);
-        int x, y;
-        
-        for (x = 0; x < MATRIX_MAX; x++) {
-            for (y = MATRIX_MAX - 1; y > -1; y--) {
-                if (matrix[x][y] != INF) {
-                    Node temp = (Node)malloc(sizeof(struct node));
-                    if (temp != NULL) {
-                        temp->vertex = y;
-                        temp->cost = matrix[x][y];
-                        temp->edge = (*L)[x];
-                        (*L)[x] = temp;
-                    }
-                }
-            }
-        }
-    }
-
-    return L;
+    // Insert code here
 }
 
 /************************************************************
@@ -129,34 +75,12 @@ void displayVHeap(VHeap VH) {
     printf("\n%10s%10s%10s%15s\n", "-----", "------", "----", "----------");
 
     // Insert code here
-    int x;
-    for (x = 0; x < VH_MAX; x++) {
-        printf("%10d", x);
-        printf("%10d", VH->nodes[x].vertex);
-        printf("%10d", VH->nodes[x].cost);
-        printf("%10d\n", VH->nodes[x].edge);
-
-    }
 
     printf("\n\n");
     system("Pause");
 }
 VHeap initVHeap() {
-    VHeap VH = (VHeap)malloc(sizeof(struct heap));
-
-    if (VH != NULL) {
-        int x;
-
-        for (x = 0; x < VH_MAX; x++) {
-            VH->nodes[x].vertex = -1;
-            VH->nodes[x].cost = -1;
-            VH->nodes[x].edge = x - 1;
-        }
-
-        VH->avail = x - 1;
-    }
-
-    return VH;
+    // Insert code here
 }
 
 /************************************************************
@@ -170,58 +94,24 @@ void displayCursor(CURSOR cursor) {
     printf("\n%-7s%15s\n", "------", "--------------------");
 
     // Insert code here
-    int group, trav;
-    for (group = 0; group < CLOSE_MAX; group++) {
-        printf("%-7d", group);
-        for (trav = cursor.header[group]; trav != -1; trav = cursor.VH->nodes[trav].edge) {
-            printf("%-2d (%-3d) | ", cursor.VH->nodes[trav].vertex, cursor.VH->nodes[trav].cost);
-        }
-        printf("\n");
-    }
+
     printf("\n\n");
     system("Pause");
 }
 void initCursor(CURSOR *CS, VHeap VH) {
-    CS->VH = VH;
-
-    int x;
-    for (x = 0; x < CLOSE_MAX; x++) {
-        CS->header[x] = -1;
-    }
+    // Insert code here
 }
 
 /************************************************************
  *  Problem 6:: Function Definitions                          *
  ************************************************************/
 void convertToCursor(LIST *L, CURSOR *CS) {
-    int group;
-    Node trav;
-    for (group = 0; group < OPEN_MAX; group++) {
-        int *cTrav = &CS->header[group];
-        for (trav = (*L)[group]; trav != NULL; trav = trav->edge) {
-            if (CS->VH->avail != -1) {
-                int temp = CS->VH->avail;
-                CS->VH->avail = CS->VH->nodes[temp].edge;
-
-                CS->VH->nodes[temp].vertex = trav->vertex;
-                CS->VH->nodes[temp].cost = trav->cost;
-                CS->VH->nodes[temp].edge = -1;
-                *cTrav = temp;
-                cTrav = &CS->VH->nodes[temp].edge;
-            }
-        }
-    }
+    // Insert code here
 }
 
 /************************************************************
  *  Problem 7:: Function Definitions                          *
  ************************************************************/
 void convertToMatrix(LIST *L, MATRIX matrix) {
-    int group;
-    Node trav;
-    for (group = 0; group < OPEN_MAX; group++) {
-        for (trav = (*L)[group]; trav != NULL; trav = trav->edge) {
-            matrix[group][trav->vertex] = trav->cost;
-        }
-    }
+    // Insert code here
 }
